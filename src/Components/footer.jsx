12d31180e26867804aa8547663ca66ec45890231
Shivraj Kolwankar <120171741+shivraj0124@users.noreply.github.com>
2023-04-07 18:07:25 +0530

    Update footer.jsx

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="blog-footer p-4 w-100" style={{ backgroundColor: '#2B3467',position:'absolute',left:'0',right:'0',bottom:'0'}}>
       <center>
 <h6 style={{ color: '#fff' }} > &copy; ImgGallery 2023</h6>  
-        <h5 style={{ color: '#fff' }} >Shivraj Kolwankar</h5>  
+        <h5 style={{ color: '#fff' }} >&#9829; Shivraj Kolwankar &#9829;</h5>  
         </center>
       </footer>
   )
