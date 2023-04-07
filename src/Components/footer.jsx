5eae22c527e68cd374f0b3a89bdc906240d4ef47
Shivraj Kolwankar <120171741+shivraj0124@users.noreply.github.com>
2023-04-07 18:05:24 +0530

    Update footer.jsx

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 function Footer() {
   return (
-    <footer className="blog-footer d-flex justify-content-center p-4 w-100" style={{ backgroundColor: '#2B3467',position:'absolute',left:'0',right:'0',bottom:'0'}}>
-      <h5 style={{ color: '#fff' }} >  &copy; ImgGallery 2023</h5>  
-        <h5 style={{ color: '#fff' }} ><br/><br/>Shivraj Kolwankar</h5>  
-        
+    <footer className="blog-footer p-4 w-100" style={{ backgroundColor: '#2B3467',position:'absolute',left:'0',right:'0',bottom:'0'}}>
+      <center>
+<h6 style={{ color: '#fff' }} > &copy; ImgGallery 2023</h6>  
+        <h5 style={{ color: '#fff' }} >Shivraj Kolwankar</h5>  
+        </center>
       </footer>
   )
 }
